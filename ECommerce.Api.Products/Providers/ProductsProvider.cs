2f00260a676b19fd7501041b54a4c492e8a609c9
andersonmorony <andersonmoroni92@gmail.com>
2022-05-20 09:46:41 -0300

    feat: create the product microservice, inprove produducts provider, database config and anothers configurations

---
+++ ECommerce.Api.Products/Providers/ProductsProvider.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Interfaces;
+using ECommerce.Api.Products.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Providers
+{
+    public class ProductsProvider : IProductsProvider
+    {
+        private readonly ProductsDbContext _dbContext;
+        private readonly ILogger<ProductsProvider> _logger;
+        private readonly IMapper _mapper;
+
+        public ProductsProvider(ProductsDbContext dbContext, ILogger<ProductsProvider> logger, IMapper mapper )
+        {
+            this._dbContext = dbContext;
+            this._logger = logger;
+            this._mapper = mapper;
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if(!_dbContext.Products.Any())
+            {
+                _dbContext.Products.Add(new Db.Product() { Id = 1, Name = "Keyboard", Price = 20, Inventary = 100 });
+                _dbContext.Products.Add(new Db.Product() { Id = 2, Name = "Mouse", Price = 5, Inventary = 200 });
+                _dbContext.Products.Add(new Db.Product() { Id = 1, Name = "Monitor", Price = 100, Inventary = 150 });
+                _dbContext.Products.Add(new Db.Product() { Id = 1, Name = "CPU", Price = 200, Inventary = 1000 });
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
