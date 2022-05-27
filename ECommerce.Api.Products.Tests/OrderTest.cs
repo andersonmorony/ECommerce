ede726ff867712ad267ade607fa9280be3f5097b
andersonmorony <andersonmoroni92@gmail.com>
2022-05-27 10:09:32 -0300

    feat: create test to products

---
+++ ECommerce.Api.Products.Tests/OrderTest.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using ECommerce.Api.Orders.Db;
+using ECommerce.Api.Orders.Profiles;
+using ECommerce.Api.Orders.Providers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ECommerce.Api.Products.Tests
+{
+    public class OrderTest
+    {
+        [Fact]
+        public async Task GetOrdersShouldReturnAllOrders()
+        {
+            //Dbcontext
+            var options = new DbContextOptionsBuilder<OrdersDbContext>().UseInMemoryDatabase(nameof(GetOrdersShouldReturnAllOrders)).Options;
+            var dbContext = new OrdersDbContext(options);
+            createOrders(dbContext);
+
+            //Profile
+            var orderProfile = new OrderProfile();
+            var config = new MapperConfiguration(provider => provider.AddProfile(orderProfile));
+            var mapper = new Mapper(config);
+
+            //Provider
+            var orderProvider = new OrdersProvider(dbContext, null, mapper);
+            var ordersResult = await orderProvider.getOrdersAsync();
+
+            Assert.True(ordersResult.isSuccess);
+            Assert.True(ordersResult.Orders.Any());
+            Assert.Null(ordersResult.ErrorMessage);
+
+        }
+
+        private void createOrders(OrdersDbContext dbContext)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                dbContext.Orders.Add(new Order()
+                {
+                    Id = i,
+                    CustomerId = i + 1,
+                    OrderDate = DateTime.Now,
+                    Total = i * 8,
+                    Items = new List<OrderItem>() {
+                        new OrderItem() { Id = i, OrderId = i, ProductId = i , Quantity = i * 100, UnitPrice = (decimal)i*3 },
+                        new OrderItem() { Id = i + 100, OrderId = i, ProductId = i , Quantity = i * 50, UnitPrice = (decimal)i*3 },
+                        new OrderItem() { Id = i + 200, OrderId = i, ProductId = i , Quantity = i * 40, UnitPrice = (decimal)i*3 },
+                        new OrderItem() { Id = i + 300, OrderId = i, ProductId = i , Quantity = i * 7, UnitPrice = (decimal)i*3 }
+                    }
+                });
+            }
+            dbContext.SaveChanges();
+        }
+    }
+}
--- ECommerce.Api.Products.Tests/ProductsTest.cs
+++ ECommerce.Api.Products.Tests/ProductsTest.cs
@@ -10,21 +10,24 @@ using Xunit;
 
 namespace ECommerce.Api.Products.Tests
 {
-    public class UnitTest1
+    public class ProductsTest
     {
         [Fact]
         public async Task GetProductsReturnsAllProducts()
         {
+            //Dbcontext
             var options = new DbContextOptionsBuilder<ProductsDbContext>().UseInMemoryDatabase(nameof(GetProductsReturnsAllProducts)).Options;
             var dbContext = new ProductsDbContext(options);
             CreateProducts(dbContext);
 
+            //Mapper
             var productProfile = new ProductProfile();
             var configuration = new MapperConfiguration(cfg => cfg.AddProfile(productProfile));
             var mapper = new Mapper(configuration);
 
             var productsProvider = new ProductsProvider(dbContext, null, mapper);
 
+            //Provider
             var products = await productsProvider.GetProductsAsync();
 
             Assert.True(products.IsSuccess);
